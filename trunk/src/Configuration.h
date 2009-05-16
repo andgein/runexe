4222c676b4a213cb216461e3bd3ad7254380c61d
Levshunov.D <Levshunov.D@e5b7a2ee-37cd-11de-8c0b-2fbac4a05959>
2009-05-16 09:05:55 +0000

    Return exit code 0 if invocation crashed and option -x not specified.

--- trunk/src/Configuration.h
+++ trunk/src/Configuration.h
@@ -6,7 +6,7 @@
 namespace runexe
 {
     const int RUN_EXIT_FAILURE = 2147483647;
-    const std::string version = "0.0.7";
+    const std::string version = "0.0.8";
     const std::string copyrightYears = "2009";
 
     const bool RETURN_EXIT_CODE = false;
